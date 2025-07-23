801bf43d2ef02b8f2625bb8c6fb70edecdf59432
rhovelz <r4divanhanief@gmail.com>
2025-07-23 16:56:35 +0700

    LFG

--- Rh0v3lz-gs.py
+++ Rh0v3lz-gs.py
@@ -12,7 +12,7 @@ def cari_keyword_di_google(keyword, jumlah_hasil=50):
 
 # Contoh penggunaan
 if __name__ == "__main__":
-    keyword = "bankaceh"
+    keyword = "fill-it"
     hasil_pencarian = cari_keyword_di_google(keyword)
     for i, url in enumerate(hasil_pencarian, start=1):
         print(f"{i}. {url}")
